fc6bb61bf006402b78acd4b599b14e06bd42e2d3
Viswak23 <Viswak.RameshBalakrishnan@metropolia.fi>
2024-09-10 18:20:17 +0530

    added register page

--- sprint-2/project-frontend/src/pages/home.jsx
+++ sprint-2/project-frontend/src/pages/home.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import Header from '../components/Header';
+
 import Hero from '../components/Hero';
 // import Tabs from './components/Tabs';
 import NewReleases from '../components/NewReleases';
 import TopHits from '../components/TopHits';
 import BrowseByGenre from '../components/BrowseByGenre';
-import Footer from '../components/Footer';
+
 
 function Home() {
   return (
     <div>
-        <Header />
+        
         <Hero />
         {/* <Tabs /> */}
         <NewReleases />
         <TopHits/>
         <BrowseByGenre />
-        <Footer />
+        
     </div>
   )
 }
