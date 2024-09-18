4cf808de8b507f8dd2203a80b5c740dc36063242
Viswak23 <Viswak.RameshBalakrishnan@metropolia.fi>
2024-09-18 13:31:10 +0530

    Changes Class Names for Results

--- sprint-2/project-frontend/src/components/SearchResults.jsx
+++ sprint-2/project-frontend/src/components/SearchResults.jsx
@@ -9,23 +9,22 @@ function SearchResults() {
         { id: 4, title: 'Result Four', imgSrc: 'https://via.placeholder.com/150' },
         { id: 5, title: 'Result Five', imgSrc: 'https://via.placeholder.com/150' },
         { id: 6, title: 'Result Six', imgSrc: 'https://via.placeholder.com/150' },
-        { id: 7, title: 'Result seven', imgSrc: 'https://via.placeholder.com/150' },
+        { id: 7, title: 'Result Seven', imgSrc: 'https://via.placeholder.com/150' },
         { id: 8, title: 'Result Eight', imgSrc: 'https://via.placeholder.com/150' },
         { id: 9, title: 'Result Nine', imgSrc: 'https://via.placeholder.com/150' },
         { id: 10, title: 'Result Ten', imgSrc: 'https://via.placeholder.com/150' },
         { id: 11, title: 'Result Eleven', imgSrc: 'https://via.placeholder.com/150' },
         { id: 12, title: 'Result Twelve', imgSrc: 'https://via.placeholder.com/150' },
-        { id: 13, title: 'Result Thirteeen', imgSrc: 'https://via.placeholder.com/150' },
-        
+        { id: 13, title: 'Result Thirteen', imgSrc: 'https://via.placeholder.com/150' },
     ];
 
     return (
-        <div className="top-hits">
-            <h2 className="top-hits-title">Results ({songs.length})</h2>
-            <div className="top-hits-list">
+        <div className="search-results">
+            <h2 className="search-results-title">Results ({songs.length})</h2>
+            <div className="search-results-list">
                 {songs.map(song => (
                     <div className="song-card" key={song.id}>
-                        <img className="song-image"  src={song.imgSrc} alt={song.title}/>
+                        <img className="song-image" src={song.imgSrc} alt={song.title} />
                         <p className="song-title">{song.title}</p>
                     </div>
                 ))}
