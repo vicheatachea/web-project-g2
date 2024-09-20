753d6cf76369dcc51ad06fbdacf8749eddba49b2
SakuJuuH <heinonensaku@gmail.com>
2024-09-20 16:44:25 +0300

    Add User Authentication and Authorization with jwt and add password encryption with bcrypt

--- sprint-3/backend/controllers/userController.js
+++ sprint-3/backend/controllers/userController.js
@@ -1,16 +1,35 @@
 const User = require("../models/userModel");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const jwt_secret_key = process.env.JWT_SECRET;
 
 const getUserData = async (req) => {
 	try {
-		const userData = await User.findOne({
-			email: req.body.email,
-			password: req.body.password,
-		}).exec();
-		if (!userData) {
-			return null;
+		const userId = req.user ? req.user.userId : null;
+		const { email } = req.body;
+		//console.log(email, userId);
+
+		if (!userId) {
+			const userData = await User.findOne({
+				email: email,
+			}).exec();
+			if (!userData) {
+				return null;
+			} else {
+				return userData;
+			}
 		} else {
-			return userData;
+			const userData = await User.findOne({
+				_id: userId,
+			}).exec();
+			if (userData) {
+				return userData;
+			} else {
+				return null;
+			}
 		}
 	} catch (error) {
 		console.error("Error fetching user data:", error);
@@ -18,14 +37,31 @@ const getUserData = async (req) => {
 	}
 };
 
-const createUser = async (req, res) => {
+const HashPassword = async (password) => {
+	const hashedPassword = await bcrypt.hash(password, 10);
+	return hashedPassword;
+};
+
+const registerUser = async (req, res, next) => {
 	try {
-		const userData = await getUserData(req);
-		if (userData !== null) {
+		const { username, email, password, role } = req.body;
+		//console.log(req.body);
+
+		const userData = await getUserData(req, res);
+		//console.log(userData);
+
+		if (userData) {
 			return res.status(400).json({ message: "User already exists" });
 		}
-		const newUser = await User.create({ ...req.body });
-		res.status(201).json(newUser);
+
+		const newUser = await User.create({
+			username,
+			email,
+			password: await HashPassword(password),
+			role,
+		});
+
+		loginUser(req, res, next, newUser, password);
 	} catch (error) {
 		if (error instanceof mongoose.Error.ValidationError) {
 			res.status(400).json({
@@ -41,20 +77,65 @@ const createUser = async (req, res) => {
 	}
 };
 
-const updateUser = async (req, res) => {
-	const { email, password } = req.body;
-	console.log(email, password);
+const loginUser = async (
+	req,
+	res,
+	next /* idk why but it doesn't work without */,
+	user = null,
+	unHashedPassword = null
+) => {
+	try {
+		//console.log('user:', user);
+		//console.log('req.body:', req.body);
 
-	if (!email || !password) {
-		return res
-			.status(400)
-			.json({ message: "Email or password are required!" });
+		const { email, password } = user ? user : req.body;
+
+		if (!email || (!password && !unHashedPassword)) {
+			return res
+				.status(400)
+				.json({ message: "Email and password are required!" });
+		}
+		const userData = user ? user : await getUserData(req, res);
+
+		//console.log(userData);
+		if (!userData) {
+			return res.status(404).json({ message: "User not found" });
+		}
+
+		const passwordMatch = await bcrypt.compare(
+			unHashedPassword ? unHashedPassword : password,
+			userData.password
+		);
+		//console.log(passwordMatch);
+		const usernameMatch = email === userData.email;
+		if (!passwordMatch || !usernameMatch) {
+			return res
+				.status(401)
+				.json({ message: "Invalid password or email!" });
+		}
+
+		const token = jwt.sign({ userId: userData._id }, jwt_secret_key, {
+			expiresIn: "1h",
+		});
+		res.cookie("jwt", token, { httpOnly: true, secure: true });
+
+		console.log("Login Successful", token);
+		res.status(200).json({ message: "Login successful", token: token });
+	} catch (error) {
+		res.status(500).json({ Error: error.message });
+	}
+};
+
+const updateUser = async (req, res) => {
+	const { username, email, password, role } = req.body;
+
+	if (!email) {
+		return res.status(400).json({ message: "Email is required!" });
 	}
 
 	const userData = await getUserData(req, res);
-	console.log(userData);
 
-	if (userData === null) {
+	if (!userData) {
 		return res.status(404).json({ message: "User not found" });
 	}
 
@@ -65,9 +146,14 @@ const updateUser = async (req, res) => {
 	}
 
 	try {
+		let updatedData = { username, email, password, role };
+		if (password) {
+			updatedData.password = await HashPassword(password);
+		}
+
 		const updatedUser = await User.findOneAndUpdate(
 			{ _id: userId },
-			{ ...req.body },
+			updatedData,
 			{ new: true }
 		);
 		if (updatedUser) {
@@ -91,17 +177,8 @@ const updateUser = async (req, res) => {
 };
 
 const deleteUser = async (req, res) => {
-	const { email, password } = req.body;
-	console.log(email, password);
-
-	if (!email || !password) {
-		return res
-			.status(400)
-			.json({ message: "Email or password are required!" });
-	}
-
 	const userData = await getUserData(req, res);
-	console.log(userData);
+	//console.log(userData);
 
 	if (userData === null) {
 		return res.status(404).json({ message: "User not found" });
@@ -132,7 +209,8 @@ const deleteUser = async (req, res) => {
 };
 
 module.exports = {
-	createUser,
+	registerUser,
+	loginUser,
 	updateUser,
 	deleteUser,
 };
