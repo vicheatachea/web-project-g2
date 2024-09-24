9441fa4675630afbf260282d585778fb24fa0246
SakuJuuH <heinonensaku@gmail.com>
2024-09-24 18:58:15 +0300

    Add submit functionality to search bar with enter key

--- sprint-3/frontend/src/components/Header.jsx
+++ sprint-3/frontend/src/components/Header.jsx
@@ -1,40 +1,90 @@
-import React from 'react';
-import './Header.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
-import { faBell, faUser, faBars } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faUser, faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header({ theme, toggleTheme }) {
-    const [dropdownVisible, setDropdownVisible] = React.useState(false);
+	const [searchQuery, setSearchQuery] = useState("");
+	const [dropdownVisible, setDropdownVisible] = useState(false);
+    const navigate = useNavigate();
 
-    const toggleDropdown = () => {
-        setDropdownVisible(!dropdownVisible);
+	const toggleDropdown = () => {
+		setDropdownVisible(!dropdownVisible);
+	};
+
+    const handleInputChange = (e) => {
+        setSearchQuery(e.target.value);
     };
+	
+    const handleKeyDown = async (e) => {
+        if (e.key === 'Enter' && searchQuery) {
+            e.preventDefault();
+            console.log('Search query:', searchQuery);
+        }
+        
+		/*
+        try {
+			const response = await searchSpotify(searchQuery);
+			console.log("Response:", response);
+			Navigate(`/search/"${searchQuery}`);
+		} catch (err) {
+			console.error("Error:", err["response"]["data"]);
+		}
+        */
+	};
+    
 
-    return (
-        <header className={`sickbeat-header ${theme}`}>
-            <a href='/' className="logo">SickBeat</a>
-            <div className="search-container">
-                <input type="text" placeholder="Search..."/>
+	return (
+		<header className={`sickbeat-header ${theme}`}>
+			<a href='/' className='logo'>
+				SickBeat
+			</a>
+			<div className='search-container'>
+                <input
+                    type='text'
+                    placeholder='Search...'
+                    value={searchQuery}
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                     
+                />
             </div>
-            <nav className="navbar">
-                <ul className="icon-list">
-                    <li><a href=""><FontAwesomeIcon icon={faBell} size="lg" /></a></li>
-                    <li><a href="/login"><FontAwesomeIcon icon={faUser} size='lg'/></a></li>
-                    <li>
-                        <a href="#" onClick={toggleDropdown}>
-                            <FontAwesomeIcon icon={faBars} size='lg'/>
-                        </a>
-                        {dropdownVisible && (
-                            <div className="dropdown-menu">
-                                <button className="mode-button" onClick={toggleTheme}>Light/Dark</button>
-                                <button className="my-profile-button">My profile</button>
-                            </div>
-                        )}
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+			<nav className='navbar'>
+				<ul className='icon-list'>
+					<li>
+						<a href=''>
+							<FontAwesomeIcon icon={faBell} size='lg' />
+						</a>
+					</li>
+					<li>
+						<a href='/login'>
+							<FontAwesomeIcon icon={faUser} size='lg' />
+						</a>
+					</li>
+					<li>
+						<a href='#' onClick={toggleDropdown}>
+							<FontAwesomeIcon icon={faBars} size='lg' />
+						</a>
+						{dropdownVisible && (
+							<div className='dropdown-menu'>
+								<button
+									className='mode-button'
+									onClick={toggleTheme}
+								>
+									Light/Dark
+								</button>
+								<button className='my-profile-button'>
+									My profile
+								</button>
+							</div>
+						)}
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
