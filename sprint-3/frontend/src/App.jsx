332bb1f4898a3bd99345f9a5d8956c8ed342aa71
SakuJuuH <heinonensaku@gmail.com>
2024-09-27 16:11:51 +0300

    fix navigate error

--- sprint-3/frontend/src/App.jsx
+++ sprint-3/frontend/src/App.jsx
@@ -1,45 +1,46 @@
-import {useState, useEffect} from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import HomePage from './pages/HomePage.jsx';
-import './App.css';
-import SearchResultsPage from './pages/SearchResultsPage.jsx';
-import MusicPlayerPage from './pages/MusicPlayerPage.jsx';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage.jsx";
+import "./App.css";
+import SearchResultsPage from "./pages/SearchResultsPage.jsx";
+import MusicPlayerPage from "./pages/MusicPlayerPage.jsx";
+
 function App() {
-    const [theme, setTheme] = useState(() => {
-        return localStorage.getItem('theme') || 'light';
-    });
+	const [theme, setTheme] = useState(() => {
+		return localStorage.getItem("theme") || "light";
+	});
 
-    useEffect(() => {
-        document.body.className = theme;
-        localStorage.setItem('theme', theme);
-    }, [theme]);
+	useEffect(() => {
+		document.body.className = theme;
+		localStorage.setItem("theme", theme);
+	}, [theme]);
 
-    const toggleTheme = () => {
-        const newTheme = theme === 'light' ? 'dark' : 'light';
-        setTheme(newTheme);
-        document.body.className = newTheme; // Apply the theme to the body
-    };
+	const toggleTheme = () => {
+		const newTheme = theme === "light" ? "dark" : "light";
+		setTheme(newTheme);
+		document.body.className = newTheme; // Apply the theme to the body
+	};
 
-    return (
-        <div className={`app ${theme}`}>
-            <Header theme={theme} toggleTheme={toggleTheme}/>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<HomePage/>}/>
-                    <Route path='/login' element={<LoginPage/>}/>
-                    <Route path='/register' element={<RegisterPage/>}/>
-                    <Route path='/search*' element={<SearchResultsPage />} />
-                    <Route path='/player' element={<MusicPlayerPage />} />
-                </Routes>
-            </BrowserRouter>
-            <Footer theme={theme}/>
-        </div>
-    );
+	return (
+		<>
+			<BrowserRouter>
+				<Header theme={theme} toggleTheme={toggleTheme} />
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/login' element={<LoginPage />} />
+					<Route path='/register' element={<RegisterPage />} />
+					<Route path='/search' element={<SearchResultsPage />} />
+					<Route path='/player' element={<MusicPlayerPage />} />
+				</Routes>
+			</BrowserRouter>
+			<Footer theme={theme} />
+		</>
+	);
 }
 
 export default App;
