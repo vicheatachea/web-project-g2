b283437a4e9d8e31c8db1d46c1de0c79b81c2279
vicheatachea <vicheata.chea@metropolia.fi>
2024-10-01 14:45:44 +0300

    merged to current changes

--- sprint-3/frontend/src/components/Header.jsx
+++ sprint-3/frontend/src/components/Header.jsx
@@ -5,16 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell, faUser, faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 function Header({ theme, toggleTheme }) {
-<<<<<<< HEAD
-=======
-    const [dropdownVisible, setDropdownVisible] = React.useState(false);
-    const navigate = useNavigate();
-
-    const toggleDropdown = () => {
-        setDropdownVisible(!dropdownVisible);
-    };
-
->>>>>>> frontend-functionality
     return (
         <header className={`${styles.sickbeatHeader} ${theme}`}>
             <a href='/' className={styles.logo}>SickBeat</a>
@@ -26,7 +16,6 @@ function Header({ theme, toggleTheme }) {
                     <li><a href=""><FontAwesomeIcon icon={faBell} size="lg" /></a></li>
                     <li><a href="/login"><FontAwesomeIcon icon={faUser} size='lg'/></a></li>
                     <li>
-<<<<<<< HEAD
                         <button className="mode-button" onClick={toggleTheme}>
                             {theme === 'light' ? (
                                 <FontAwesomeIcon icon={faMoon} size="lg" />
@@ -34,18 +23,6 @@ function Header({ theme, toggleTheme }) {
                                 <FontAwesomeIcon icon={faSun} size="lg" />
                             )}
                         </button>
-=======
-                        <a href="#" onClick={toggleDropdown}>
-                            <FontAwesomeIcon icon={faBars} size='lg'/>
-                        </a>
-                        {dropdownVisible && (
-                            <div className={styles.dropdownMenu}>
-                                <button className="mode-button" onClick={toggleTheme}>Light/Dark</button>
-                                <button className="library-button" onClick={() => navigate('/library')}>My Library</button>
-                                <button className="account-button" onClick={() => navigate('/account')}>My Account</button>
-                            </div>
-                        )}
->>>>>>> frontend-functionality
                     </li>
                 </ul>
             </nav>
--- sprint-3/frontend/src/components/MusicPlayer.jsx
+++ sprint-3/frontend/src/components/MusicPlayer.jsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import React, { useState, useEffect, useRef } from 'react';
-import './MusicPlayer.css';
-=======
 import React, { useState, useEffect } from 'react';
 import styles from './MusicPlayer.module.css'; 
->>>>>>> frontend-functionality
 
 const MusicPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -45,26 +40,6 @@ const MusicPlayer = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="music-player">
-      <div className="now-playing">Now Playing</div>
-      <div className="image-container">
-        <img src={song.imageUrl} alt="Artist" className="artist-image" />
-      </div>
-      <div className="song-title">
-        {song.name}
-        <span role="img" aria-label="motivation"></span>
-      </div>
-
-      <audio 
-        ref={audioRef} 
-        src={song.audioUrl} 
-        onTimeUpdate={handleTimeUpdate} 
-        onEnded={handleAudioEnd}
-      />
-
-      <div className="progress-bar-container">
-=======
     <div className={styles.musicPlayer}>
       <div className={styles.nowPlaying}>Now Playing</div>
       <div className={styles.imageContainer}>
@@ -75,28 +50,17 @@ const MusicPlayer = () => {
         <span role="img" aria-label="motivation">🔥</span>
       </div>
       <div className={styles.progressBarContainer}>
->>>>>>> frontend-functionality
         <input
           type="range"
           value={progress}
           step="0.1"
           max="100"
-<<<<<<< HEAD
-          onChange={handleSeek}
-          className="progress-bar"
-        />
-      </div>
-
-      <div className="controls">
-        <button onClick={() => audioRef.current.currentTime = 0} className="control-button">
-=======
           onChange={e => setProgress(e.target.value)}
           className={styles.progressBar}
         />
       </div>
       <div className={styles.controls}>
         <button onClick={() => setProgress(0)} className={styles.controlButton}>
->>>>>>> frontend-functionality
           ⏪
         </button>
         
@@ -104,11 +68,7 @@ const MusicPlayer = () => {
           {isPlaying ? '⏸️' : '▶️'}
         </button>
 
-<<<<<<< HEAD
-        <button onClick={() => audioRef.current.currentTime = audioRef.current.duration} className="control-button">
-=======
         <button onClick={() => setProgress(100)} className={styles.controlButton}>
->>>>>>> frontend-functionality
           ⏩
         </button>
       </div>
