87a1036b96b2e371c4749e01885dec53e95da27e
SakuJuuH <heinonensaku@gmail.com>
2024-10-01 19:23:44 +0300

    add axios calls for getUserData, updateUser and deleteUser

--- sprint-3/frontend/src/utils/userRequests.js
+++ sprint-3/frontend/src/utils/userRequests.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+export const getUser = async () => {
+    return axios.get("/api/user/data");
+};
+
 export const registerUser = async (userData) => {
 	const response = await axios.post("/api/user/register", userData);
 	return response;
@@ -9,3 +13,13 @@ export const loginUser = async (userData) => {
 	const response = await axios.post("/api/user/login", userData);
 	return response;
 };
+
+export const updateUser = async (userData) => {
+    const response = await axios.patch("/api/user/update", userData);
+    return response;
+}
+
+export const deleteUser = async (userId) => {
+    const response = await axios.delete("/api/user/delete");
+    return response;
+}
