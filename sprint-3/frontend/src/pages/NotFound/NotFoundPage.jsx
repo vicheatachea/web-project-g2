a4cd1832deeed3373f2f89b22bb9767f032f2777
IsNotRicardo <desousa.itsnotricardo@gmail.com>
2024-10-05 16:32:25 +0300

    Restyle NotFoundPage

--- sprint-3/frontend/src/pages/NotFound/NotFoundPage.jsx
+++ sprint-3/frontend/src/pages/NotFound/NotFoundPage.jsx
@@ -1,15 +1,18 @@
-import {Link} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import {FaExclamationTriangle} from 'react-icons/fa';
-import styles from './NotFoundPage.module.css'
+import styles from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
+    const navigate = useNavigate();
+
     return (
-        <section className={styles.notFoundContainer}>
-            <FaExclamationTriangle className={styles.triangle}/>
-            <h1 className={styles.notFound}>404 Not Found</h1>
-            <p className={styles.notFoundPage}>This page does not exist</p>
-            <Link className={styles.backHome} to='/'>Go Back</Link>
+        <section className={styles.container}>
+            <FaExclamationTriangle className={styles.icon}/>
+            <h1 className={styles.title}>404 Not Found</h1>
+            <p className={styles.message}>This page does not exist</p>
+            <button className={styles.backButton} onClick={() => navigate(-1)}>Go Back</button>
         </section>
     );
 };
+
 export default NotFoundPage;
