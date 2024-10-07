e364d3fd2cfc39fb64cd7100bd0bb3be351ef5b9
Viswak23 <Viswak.RameshBalakrishnan@metropolia.fi>
2024-10-07 15:23:03 +0300

    added logo

--- sprint-3/frontend/src/components/Header.jsx
+++ sprint-3/frontend/src/components/Header.jsx
@@ -4,6 +4,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faBell, faUser, faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import {useNavigate} from "react-router-dom";
 import {useState} from "react";
+import logo from '../images/logo.png';
 
 function Header({theme, toggleTheme}) {
     const [searchQuery, setSearchQuery] = useState("");
@@ -22,32 +23,36 @@ function Header({theme, toggleTheme}) {
 
     return (
         <header className={`${styles.sickbeatHeader} ${theme}`}>
-            <a href='/' className={styles.logo}>SickBeat</a>
-            <div className={styles.searchContainer}>
-                <input
-                    type='text'
-                    placeholder='Search...'
-                    value={searchQuery}
-                    onChange={handleInputChange}
-                    onKeyDown={handleKeyDown}
-                />
-            </div>
-            <nav className={styles.navbar}>
-                <ul className={styles.iconList}>
-                    <li><a href=""><FontAwesomeIcon icon={faBell} size="lg"/></a></li>
-                    <li><a href="/login"><FontAwesomeIcon icon={faUser} size='lg'/></a></li>
-                    <li>
-                        <button className="mode-button" onClick={toggleTheme}>
-                            {theme === 'light' ? (
-                                <FontAwesomeIcon icon={faMoon} size="lg" />
-                            ) : (
-                                <FontAwesomeIcon icon={faSun} size="lg" />
-                            )}
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+    <a href='/' className={styles.logoContainer}>
+        <img src={logo} alt='SickBeat Logo' className={styles.logo} />
+        <span className={styles.logoText}>SickBeat</span>
+    </a>
+    <div className={styles.searchContainer}>
+        <input
+            type='text'
+            placeholder='Search...'
+            value={searchQuery}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+        />
+    </div>
+    <nav className={styles.navbar}>
+        <ul className={styles.iconList}>
+            <li><a href=""><FontAwesomeIcon icon={faBell} size="lg"/></a></li>
+            <li><a href="/login"><FontAwesomeIcon icon={faUser} size='lg'/></a></li>
+            <li>
+                <button className="mode-button" onClick={toggleTheme}>
+                    {theme === 'light' ? (
+                        <FontAwesomeIcon icon={faMoon} size="lg" />
+                    ) : (
+                        <FontAwesomeIcon icon={faSun} size="lg" />
+                    )}
+                </button>
+            </li>
+        </ul>
+    </nav>
+</header>
+
     );
 }
 
