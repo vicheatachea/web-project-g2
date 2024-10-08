3c39768e04fc041232f1c9f64d533ddc42852ef8
SakuJuuH <heinonensaku@gmail.com>
2024-10-08 15:02:47 +0300

    removes debugging lines

--- sprint-3/frontend/src/hooks/useBackend.jsx
+++ sprint-3/frontend/src/hooks/useBackend.jsx
@@ -1,25 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 export const useBackend = () => {
 	const [data, setData] = useState(null);
-	
 	const [error, setError] = useState(null);
 
 	const sendRequest = async (url, method, requestData = null) => {
-        try {
-            console.log(requestData)
-            const response = await axios({ method, url, data: requestData });
-            console.log(response);
-            setData(response);
-            return response
-        } catch (error) {
-            console.log(error);
+		try {
+			const response = await axios({ method, url, data: requestData });
+			setData(response);
+			return response;
+		} catch (error) {
 			if (error.response) {
 				setError(error.response.data);
 				return error.response.data;
-            } else {
-				setError(error.message);
+			} else {
 				return { message: error.message };
 			}
 		}
