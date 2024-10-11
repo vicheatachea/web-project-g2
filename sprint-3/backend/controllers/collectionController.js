2edf56cb7e4e3e1c148a191d47b75268388b8bdc
IsNotRicardo <desousa.itsnotricardo@gmail.com>
2024-10-11 16:38:19 +0300

    Fix security flaw in createCollection
    All user ids of that collection were being returned

--- sprint-3/backend/controllers/collectionController.js
+++ sprint-3/backend/controllers/collectionController.js
@@ -5,67 +5,68 @@ const getAllCollections = async (req, res) => {
     const userId = req.user.userId;
 
     try {
-        const collections = await Collection.find({ userIds: userId }, 'name id type songAmount image');
+        const collections = await Collection.find({userIds: userId}, 'name id type songAmount image');
         res.status(200).json(collections);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({message: error.message});
     }
 };
 
 // GET a specific collection by ID
 const getCollectionById = async (req, res) => {
-    const { id } = req.params;
+    const {id} = req.params;
     const userId = req.user.userId;
 
     try {
-        const collection = await Collection.findOne({ id, userIds: userId }, '-userIds');
+        const collection = await Collection.findOne({id, userIds: userId}, '-userIds');
         if (!collection) {
-            return res.status(404).json({ message: "Collection not found" });
+            return res.status(404).json({message: "Collection not found"});
         }
         res.status(200).json(collection);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({message: error.message});
     }
 };
 
 // POST a new collection
 const createCollection = async (req, res) => {
-    const { name, id, type, songAmount, image } = req.body;
+    const {name, id, type, songAmount, image} = req.body;
     const userId = req.user.userId;
 
     try {
-        let collection = await Collection.findOne({ id });
+        let collection = await Collection.findOne({id});
 
         if (collection) {
             // If collection exists, check if user ID is already present
             if (collection.userIds.includes(userId)) {
-                return res.status(400).json({ message: "User has already added this collection" });
+                return res.status(400).json({message: "User has already added this collection"});
             } else {
                 collection.userIds.push(userId);
                 await collection.save();
             }
         } else {
             // If collection does not exist, create a new one
-            collection = new Collection({ name, id, type, songAmount, image, userIds: [userId] });
+            collection = new Collection({name, id, type, songAmount, image, userIds: [userId]});
             await collection.save();
         }
 
-        res.status(201).json(collection);
+        const responseCollection = await Collection.findOne({id}).select('-userIds');
+        res.status(201).json(responseCollection);
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        res.status(400).json({message: error.message});
     }
 };
 
 // DELETE a specific collection by ID
 const deleteCollection = async (req, res) => {
-    const { id } = req.params;
+    const {id} = req.params;
     const userId = req.user.userId;
 
     try {
-        const collection = await Collection.findOne({ id });
+        const collection = await Collection.findOne({id});
 
         if (!collection) {
-            return res.status(404).json({ message: "Collection not found" });
+            return res.status(404).json({message: "Collection not found"});
         }
 
         // Remove user ID from the collection's user array
@@ -73,15 +74,15 @@ const deleteCollection = async (req, res) => {
 
         if (collection.userIds.length === 0) {
             // If no users are associated with the collection, delete it
-            await collection.deleteOne({ id });
+            await collection.deleteOne({id});
         } else {
             // Otherwise, save the updated collection
             await collection.save();
         }
 
-        res.status(200).json({ message: "Collection updated" });
+        res.status(200).json({message: "Collection updated"});
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({message: error.message});
     }
 };
 
